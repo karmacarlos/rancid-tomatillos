76fd59ccee3239a9cefb4bd9f05a14192842ebe0
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-10-14 19:57:01 -0600

    Created React Initial structure and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { movieData } from './movieData'
+import { Header } from './Header/Header'
+// import logo from './user-icon.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = []
+    this.state = movieData;
   }
 
   render() {
     return (
       <div className="App">
-      
+      <Header />
       </div>
     )
   }
